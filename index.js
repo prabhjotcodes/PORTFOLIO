0768b11da262006cae120a1273af5673c62f5b8c
Prabhjot Singh <codebyprabh@gmail.com>
2025-08-12 17:48:45 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -23,4 +23,23 @@ $(document).ready(function () {
     $("#nav-collapse").toggle();
   });
 });
-  
+
+function animateProgressBars() {
+    $('.progress-fill').each(function () {
+      const rect = this.getBoundingClientRect();
+      const inView = rect.top < window.innerHeight && rect.bottom > 0;
+      const targetWidth = $(this).attr('data-width');
+
+      if (inView) {
+        $(this).css('width', targetWidth);
+      } else {
+        // Reset when out of view so animation can happen again
+        $(this).css('width', '0');
+      }
+    });
+  }
+
+  $(document).ready(function () {
+    animateProgressBars(); // initial run
+    $(window).on('scroll', animateProgressBars); // on scroll
+  });
